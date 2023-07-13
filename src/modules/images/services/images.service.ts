57638095dbd9cba480f596bd9e8a01c8deb944bf
Bruno Cesar de Lima de Paula <mbrunocesar@gmail.com>
2023-07-13 16:45:09 -0300

    adding file upload integration to s3

--- src/modules/images/services/images.service.ts
+++ src/modules/images/services/images.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginateRequestDto } from '@shared/base-repository/helpers/paginate-helper/dto/paginate-request.dto';
 import { IPaginate } from '@shared/base-repository/helpers/paginate-helper/i-paginate';
+import { S3 } from "aws-sdk";
+import { v4 as uuid } from 'uuid';
 import { CreateImageDto } from '../dto/create-image.dto';
 import { Image } from '../entities/image.entity';
 import { IImagesRepository } from '../repositories/i-images-repository';
@@ -17,6 +19,24 @@ export class ImagesService {
 
     return { id: image.image_id };
   }
+
+  async uploadFile(dataBuffer: Buffer, fileName: string) {
+    const s3 = new S3();
+    const uploadResult = await s3.upload({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Body: dataBuffer,
+      Key: `${uuid()}-${fileName}`,
+    }).promise();
+
+    const fileStorageInDB = ({
+      fileName: fileName,
+      fileUrl: uploadResult.Location,
+      key: uploadResult.Key,
+    });
+
+    return fileStorageInDB;
+  }
+
 
   async findAll(
     paginateRequestDto: PaginateRequestDto,
