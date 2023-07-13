2b5cd7e041531a3af88770ea929eb56c992c8f43
Bruno Cesar de Lima de Paula <mbrunocesar@gmail.com>
2023-07-13 16:52:50 -0300

    adding file upload integration to s3

--- src/modules/images/controllers/images.controller.ts
+++ src/modules/images/controllers/images.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, UploadedFile, UseInterceptors, Query } from '@nestjs/common';
+import { FileInterceptor } from "@nestjs/platform-express";
+
 import { ImagesService } from '../services/images.service';
 import { CreateImageDto } from '../dto/create-image.dto';
 import { PaginateRequestDto } from '@shared/base-repository/helpers/paginate-helper/dto/paginate-request.dto';
@@ -11,6 +13,18 @@ export class ImagesController {
   create(@Body() createImageDto: CreateImageDto) {
     return this.imagesService.create(createImageDto);
   }
+
+  @Post('/upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    if (!file) {
+      return new Error('422 - Unprocessable Entity');
+    }
+    const uploadedFile = await this.imagesService.uploadFile(file.buffer, file.originalname);
+
+    return uploadedFile;
+  }
+
 
   @Get()
   findAll(@Query() paginateRequestDto: PaginateRequestDto) {
